fe01572b3f0b43b3e1dd70dd82cfc99dfe12cbf9
nomuk984 <nomundari0804@gmail.com>
2024-09-06 20:23:39 +0800

    first commit

---
+++ words.js
@@ -0,0 +1,40 @@
+export const options = {
+    fruits: [
+        "Apple",
+        "Banana",
+        "Mango",
+        "Orange",
+        "Pineapple",
+        "Strawberry",
+        "Grapes",
+        "Watermelon",
+        "Blueberry",
+        "Peach",
+    ],
+
+    animals: [
+        "Lion",
+        "Elephant",
+        "Giraffe",
+        "Kangaroo",
+        "Dolphin",
+        "Panda",
+        "Zebra",
+        "Penguin",
+        "Tiger",
+        "Koala",
+    ],
+
+    countries: [
+        "Canada",
+        "Brazil",
+        "Japan",
+        "Germany",
+        "Australia",
+        "India",
+        "Mexico",
+        "South Africa",
+        "Italy",
+        "Egypt",
+    ],
+};
